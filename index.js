51e5f2511f8faa4361da5077f13c6c74de2b721e
Kimseng Houy <59835755+KimsengHouy@users.noreply.github.com>
2024-06-10 15:01:32 +0700

    - New server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/users-list', (req, res) => {
+  // Get complete list of users
+  const usersList = [];
+
+  // Send the usersList as a response to the client
+  res.send(usersList);
+});
+
+app.post('/real-time-notification', (req, res) => {
+	res.send('OK');
+});
+
+app.listen(PORT, () => {
+  console.log(`Express server running at http://localhost:${PORT}/`);
+});
